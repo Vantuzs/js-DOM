447a89baf430873600372ed8d1cb5393eff7f57b
Vantuz <Shcherbina444@gmail.com>
2024-11-28 11:55:53 +0200

    Practise task resolve

--- index.js
+++ index.js
@@ -5,6 +5,26 @@ form.addEventListener('submit', consoleForm);
 function consoleForm(event){
     event.preventDefault();
     const form = event.target;
-    const value = form[0].value
-    console.log(value);
+    const value = form[0].value;
+    console.log(`Hello ${value}`);
 }
+
+function helloUser(event){
+    event.preventDefault();
+    const user = event.target[0].value;
+    alert(`Hello ${user}`);
+}
+
+
+
+
+
+
+/* 
+
+Создать форму в html
+в форме спрашивать имя пользователя
+
+При отправлении формы - Поприветствуйте пользователя
+
+*/
