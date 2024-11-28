35a3ab8fd8a9c5f771c8296c132b621e918c35ff
Vantuz <Shcherbina444@gmail.com>
2024-11-28 16:04:03 +0200

    querySelector querySelectorAll

--- index.js
+++ index.js
@@ -1,27 +1,54 @@
-const collection = document.getElementsByClassName('paragraph');
+// const collection = document.getElementsByClassName('paragraph');
 
-for(let i =0; i < collection.length; i++ ){
-    collection[i].style.color = 'green'
+// for(let i =0; i < collection.length; i++ ){
+//     collection[i].style.color = 'green'
+// }
+
+
+// for(let p of collection){
+//     p.style.color = 'red'
+// }
+
+// const btns =  document.getElementsByTagName('button');
+// const arr = [...btns];
+
+
+
+// // console.log(btns);
+
+
+// function hello(event){
+//     event.target.removeEventListener('click',hello)
+//     console.log(`Hello user`);
+// }
+
+// for(let btn of btns){
+//     btn.addEventListener('click',hello)
+// }
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+const btn = document.querySelector('button');
+// console.log(btn);
+
+const btn2 = document.querySelector('.super-btn');
+// console.log(btn2);
+
+const btn3 = document.querySelector('#btnbtn');
+// console.log(btn3);
+
+const btnCollection = document.querySelectorAll('button');
+console.log(btnCollection);
+
+const paragraphCollection = document.querySelectorAll('.paragraph:nth-child(even)');
+
+for(let p of paragraphCollection){
+    p.style.backgroundColor = 'yellow';
 }
 
-
-for(let p of collection){
-    p.style.color = 'red'
-}
-
-const btns =  document.getElementsByTagName('button');
-const arr = [...btns];
-
-
-
-// console.log(btns);
-
-
-function hello(event){
-    event.target.removeEventListener('click',hello)
-    console.log(`Hello user`);
-}
-
-for(let btn of btns){
-    btn.addEventListener('click',hello)
-}
+console.log(paragraphCollection);
