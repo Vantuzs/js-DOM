d8c9e4ab35232e389b43c7f0c7072033c35956a8
Vantuz <Shcherbina444@gmail.com>
2024-12-02 14:47:55 +0200

    Practise task resolve - Darck teme

--- index.js
+++ index.js
@@ -24,3 +24,42 @@ green.addEventListener('click',greenBtnHandler)
 function greenBtnHandler(event){
     div.classList.toggle('bg-color-green')
 }
+
+
+/* 
+
+Сделать кнопку которая будет менять тему сайта
+
+Если вы нажимаете на эту кнопку - на сайте включается тёмный режим (для тега body установите backgroundColor = темно)
+а для текста = color: white;
+
+
+
+
+*/
+
+
+const body = document.querySelector('body');
+const btn = document.querySelector('#darck')
+
+btn.addEventListener('click',bodyFnc)
+
+function bodyFnc(){
+    console.log(body.classList);
+    body.classList.toggle('darck')
+    btn.classList.toggle('btnCl')
+    body.classList.forEach((a)=> {return console.log(a==='darck')})
+    
+    let o;
+    body.classList.forEach((a)=>{
+        if(a==='darck'){
+            return o=true
+        } return o = false})
+    if(o===true){
+        btn.innerHTML = 'Тёмный стиль вкл.'
+        
+    } else{
+        btn.innerHTML = 'Тёмный стиль'
+    }
+    console.log(o);
+}
