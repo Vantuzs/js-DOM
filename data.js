5f3d984c79c34f4ba1d55993aefb286294af59fb
Vantuz <Shcherbina444@gmail.com>
2024-12-05 19:14:49 +0200

    Create user card

---
+++ data.js
@@ -0,0 +1,44 @@
+const data = [
+    {
+        id: 1,
+        name: 'John',
+        description: 'Description for John',
+        profilePicture: 'https://forum-ru-cdn.warthunder.com/original/3X/a/f/af62d76a2d92797df0711e6a94d319490936f3a1.jpeg'
+    },{
+        id: 2,
+        name: 'Josh',
+        description: 'Description for Josh',
+        profilePicture: 'https://forum-ru-cdn.warthunder.com/original/3X/a/f/af62d76a2d92797df0711e6a94d319490936f3a1.jpeg'
+    },{
+        id: 3,
+        name: 'Jane',
+        description: 'Description for Jane',
+        profilePicture: 'https://forum-ru-cdn.warthunder.com/original/3X/a/f/af62d76a2d92797df0711e6a94d319490936f3a1.jpeg'
+    },
+    {
+            id: 4,
+            name: 'Iness',
+            description: 'Description for Iness',
+            profilePicture: 'https://forum-ru-cdn.warthunder.com/original/3X/a/f/af62d76a2d92797df0711e6a94d319490936f3a1.jpeg'
+        },{
+            id: 5,
+            name: 'Alex',
+            description: 'Description for Alex',
+            profilePicture: 'https://forum-ru-cdn.warthunder.com/original/3X/a/f/af62d76a2d92797df0711e6a94d319490936f3a1.jpeg'
+        },{
+            id: 6,
+            name: 'Natan',
+            description: 'Description for Natan',
+            profilePicture: 'https://forum-ru-cdn.warthunder.com/original/3X/a/f/af62d76a2d92797df0711e6a94d319490936f3a1.jpeg'
+        }, {
+            id: 7,
+        name: 'Tracy',
+        description: 'Description for Tracy',
+        profilePicture: 'https://forum-ru-cdn.warthunder.com/original/3X/a/f/af62d76a2d92797df0711e6a94d319490936f3a1.jpeg'
+        },{
+            id: 8,
+        name: 'James',
+        description: 'Description for Jame',
+        profilePicture: 'https://forum-ru-cdn.warthunder.com/original/3X/a/f/af62d76a2d92797df0711e6a94d319490936f3a1.jpeg'
+        }
+];
--- index.js
+++ index.js
@@ -1,49 +1,47 @@
-/*
+{/* <article class="card-wrapper">
+        <img class="card-image" src="https://forum-ru-cdn.warthunder.com/original/3X/a/f/af62d76a2d92797df0711e6a94d319490936f3a1.jpeg" alt="John avatar">
+        <h2 class="username">John</h2>
+        <p class="description">Description for John</p>
+      </article> */}
+     
 
-Дан section, на якій треба клацати мишею
-Дан div, який має переміститись на точку, в яку клацнули мишею
+      const root = document.querySelector('#root');
 
-*/
+      function createUserCard(user){
+        // 1. Создание article
+        const article = document.createElement('article');
+        article.classList.add('card-wrapper');
+        // 2. Создание img
+        const img = document.createElement('img');
+        img.setAttribute('src', user.profilePicture);
+        img.setAttribute('alt', `${user.name} avatar`);
+        img.classList.add('card-image');
 
+        // 3. Создание h2
+        const h2 = document.createElement('h2');
+        h2.append(user.name);
+        h2.classList.add('username');
+        // 4. Созданеи p
+        const p = document.createElement('p');
+        p.append(user.description);
+        p.classList.add('description');
 
-const field = document.querySelector('#game-field');
-const box = document.querySelector('#box');
+        // 5. Присоединить к article (п.1) елементы созданные в п. 2-4
+        article.append(img,h2,p);
 
-field.addEventListener('click',clickHandler,{capture: true});
+        // 6. Прикрепить article к section#root
+        root.append(article)
+        return article
+      }
 
-function clickHandler(event/*{clientX,clientY,target: {children: {box}}}*/){
-    // const {children: {box}}= target;
-    // console.dir(box);
+// My variant
 
-    event.stopPropagation()
-    if(event.currentTarget === event.target){
-    const {clientX,clientY,target: {children:{box}}}= event
-    box.style.left = `${clientX - (box.offsetWidth/2)}px`
-    box.style.top = `${clientY - (box.offsetHeight/2)}px` 
+    //  for(let user of data){
+    //     createUserCard(user)
+    //  }
 
+    /////////////////////////////////////////////////////////////////////////////
 
+   const cardArray = data.map(user => createUserCard(user));
 
-    if(clientX>475){
-        box.style.left = `${449}px`
-    }else{
-        box.style.left = `${clientX - (box.offsetWidth/2)}px`
-    }
-    if(clientY>475){
-        box.style.top = `${449}px`    
-    } else{
-    box.style.top = `${clientY - (box.offsetHeight/2)}px` }
-    if(clientX<25){
-        box.style.left = `${0}px`
-    }
-    // else{
-    //     box.style.left = `${clientX - (box.offsetWiidth/2)}px`
-    // }
-    if(clientY<25){
-        box.style.top = `${0}px`
-    }
-    // else{
-    //     box.style.top = `${clientY - (box.offsetHeoght/2)}px`
-    // }
-}
-    }
-    // console.log(clientX)  // console.log(clientY)
+   root.append(...cardArray)
