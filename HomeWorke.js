1f8cb32bbc36f4ccf8b20cd19ef7cbd5843c8d20
Vantuz <Shcherbina444@gmail.com>
2024-12-12 21:11:13 +0200

    RegExp

--- HomeWorke.js
+++ HomeWorke.js
@@ -1,99 +1,17 @@
-// 15. JS. Класи та ООП
+// const sec = document.querySelector('#sec');
 
+// function createInp(){
+//     const InpTxt = createElement('input',{className: ['type-text']})
+// }
 
-//1)
+// function createElement(type, {classNames}, ...childNodes) {
+//     const elem = document.createElement(type);
+//     elem.classList.add(...classNames);
+//     elem.append(...childNodes);
 
-class Boock{
-    constructor(Autor,name,year,pages,numShelf){
-        this.Autor = Autor;
-        this.name = name;
-        this.year = year;
-        this.pages = pages;
-        this.numShelf = numShelf;
-        this.idUser = null
-    }
+//     return elem;
+// }
 
-    isVacant(){
-        if(this.numShelf === null){
-            return false;
-        }
-        return true;
-        
-    }
-   
-}
+// const cardArray = data.map(createInp());
 
-class User{
-    constructor(idUser,firstName,lastName,adress){
-        this.idUser = idUser;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.adress = adress;
-        this.boockInHands = null;
-        this.boockNumShelf = null;
-    }
-
-    getBoock(boockName){
-        this.boockInHands = boockName;
-        boockName.idUser = this.idUser;
-        this.boockNumShelf = boockName.numShelf;
-        boockName.numShelf = null;
-    }
-
-    giveAvay(boockName){
-        this.boockInHands = null;
-        boockName.idUser = null;
-        boockName.numShelf = this.boockNumShelf;
-        this.boockNumShelf = null;
-    }
-}
-
-const user1 = new User(12,'John','Doe','FILIPINI 12g.');
-const boock1 = new Boock('Prostokwashin', 'LastAutumn',2000,271,3);
-
-
-//2)
-
-
-
-class Animal{
-    constructor(name,age,gender){
-        this.name = name;
-        this.age = age;
-        this.gender = gender
-    }
-
-    agr(){
-        console.log('AAAAAAAAAAAAAAAAAA');
-    }
-}
-
-
-class Tiger extends Animal{
-    constructor(name,age,gender,dlinaHvosta){
-        super(name,age,gender)
-        this.dlinaHvosta = dlinaHvosta;
-    }
-
-    agr(){
-        console.log('Я ГОЛОДЕН!!!!!! ЩА СЬЕМ ТЕБЯ ВАЩЕЕЕЕЕЕЕЕЕ');
-    }
-}
-
-class Wolf extends Animal{
-    constructor(name,age,gender){
-        super(name,age,gender)
-    }
-
-    agr(){
-        console.log('АУУУУУУУУУУУУФФФФ!!!! ЕЖЕ ГДЕ ХАВЧИК!? ');
-    }
-}
-
-const tigr1 = new Tiger('Tiger',5,'MAN');
-const tigr2 = new Tiger('Tiger',5,'MAN');
-const tigr3 = new Tiger('Tiger',5,'MAN');
-
-const wolf1 = new Wolf('Wolf',6,'MAN');
-const wolf2 = new Wolf('Wolf',6,'MAN');
-const wolf3 = new Wolf('Wolf',6,'MAN');
+// sec.append(...cardArray);
--- index.js
+++ index.js
@@ -1,103 +1,23 @@
-/* <article class="card-wrapper">
-        <div class="image-wrapper">
-          <img class="card-image" src="https://forum-ru-cdn.warthunder.com/original/3X/a/f/af62d76a2d92797df0711e6a94d319490936f3a1.jpeg" alt="John avatar">
-        </div>
-        <h2 class="username">John</h2>
-        <p class="description">Description for John</p>
-      </article> */
-     
-
-      const root = document.querySelector('#root');
-
-      function createUserCard(user){
-        //1. Создаем обвёртку для картинки
-        const imageWrapper =  createImageWrapper(user);
-
-        // 3. Создание h2
-        const h2 = createElement('h2',{classNames: ['username']}, user.name)
-        // 4. Созданеи p
-        const p = createElement('p',{classNames: ['description']}, user.description)
-
-        // 5. Создаем и возвращаем  article в которы вложены созданые img,h2,p
-        return createElement('article',{classNames: ['card-wrapper']},imageWrapper,h2,p);
-      }
-      
-      // My variant
-      
-      //  for(let user of data){
-    //     createUserCard(user)
-    //  }
-
-    /////////////////////////////////////////////////////////////////////////////
-
-   const cardArray = data.map(user => createUserCard(user));
-
-   root.append(...cardArray);
-
-   /**
-    * @param {String} type - тег елемента, котоорый нам нужно создать
-    * @param {String} classNames  - список класов которые нужно довбавить к елементу
-    * @param {...Node} childNodes - список дочирних узлов
-    * @returns {HTMLElement}
-    *  */ 
-   
-   
-   function createElement(type,{classNames},...childNodes){
-    const elem = document.createElement(type);
-    elem.classList.add(...classNames);
-    elem.append(...childNodes);
-
-    return elem;
-  }
-
-   function imageLoadHandler({target}){
-    console.log('image successfully loaded');
-    const perentWrapper = document.querySelector(`#wrapper${target.dataset.id}`)
-    perentWrapper.append(target)
-   }
-  
-   function imageErrorHandler({target}){
-    target.remove()
-   }
-
-   function createUserImage(user){
-        const img = document.createElement('img');
-        img.setAttribute('src', user.profilePicture);
-        img.setAttribute('alt', `${user.name} avatar`);
-        img.dataset.id = user.id
-        img.classList.add('card-image');
-        
-        img.addEventListener('load',imageLoadHandler)
-        img.addEventListener('error',imageErrorHandler)
-        return img
-   }
-
-   function createImageWrapper(user){
-     //1ю создание заглушки
-     const imageWrapper = createElement('div',{classNames: ['image-wrapper']});
-     imageWrapper.setAttribute('id',`wrapper${user.id}`);
-     
-    // 2. Вызначаем ,ackground0dolor заглушки с урахуванням имени пользователя
-    imageWrapper.style.backgroundColor = stringToColour(user.name)
-
-     // 2. Создание заглушки
-     const img = createUserImage(user);
-
-     return imageWrapper
-   }
+const str1 = 'hello';
+const str2 = new String('hello');
 
 
+// Regexp
 
+const reg1 = /^[a-zA-Z]{2,5}$/;
+console.log(reg1);
 
-   function stringToColour(str){
-    let hash = 0;
-    str.split('').forEach(char => {
-      hash = char.charCodeAt(0) + ((hash << 5) - hash)
-    })
-    let colour = '#'
-    for (let i = 0; i < 3; i++) {
-      const value = (hash >> (i * 8)) & 0xff
-      colour += value.toString(16).padStart(2, '0')
-    }
-    return colour
-  }
+const reg2 = new RegExp('^[a-zA-Z]{2,5}$');
+console.log(reg2);
+
+// Експерементируе: валидируеи имеил через регулярки
+
+const testString = 'sunnyday56@gmail.com';
+const emailRegExp = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/
+emailRegExp.exec(testString); // array
+emailRegExp.test(testString); // true
+
+testString.match(emailRegExp); // array -- сработаетк как exec
+
+let str = 'BLue SKY';
+str.replace(/[A-Z]{3}/,'sky'); // BLue sky
