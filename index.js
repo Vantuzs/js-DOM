60b74fcf2c1ee9cda54ff74e2344492a12b71418
Vantuz <Shcherbina444@gmail.com>
2024-12-17 12:15:17 +0200

    Then ,Catch<Finaly

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-// fetch('./user.json')
-// .then((response) => {
-//     return response.json(); // перетворюемо нули и оденички на JS объект
-// })
-// .then((data) => {
-//     console.log(data);
-// });
-
-fetch('https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5')
+fetch('./user.json')
 .then((response) => {
     return response.json(); // перетворюемо нули и оденички на JS объект
 })
 .then((data) => {
     console.log(data);
-});
+})
+.catch((err) => {
+    console.log(err);
+})
+.finally(()=> {
+    console.log('Finaly question closed');
+})
 
