5bb1a675a977a1c199348750cd08ed569e4b9d96
Vantuz <Shcherbina444@gmail.com>
2024-12-17 12:37:47 +0200

    Creating promis

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-fetch('./user.json')
-.then((response) => {
-    return response.json(); // перетворюемо нули и оденички на JS объект
-})
-.then((data) => {
-    console.log(data);
-})
-.catch((err) => {
-    console.log(err);
-})
-.finally(()=> {
-    console.log('Finaly question closed');
-})
+const p = new Promise(executor) // pending
+function executor(resolve,reject){
+    // задача: сгенерировать случайное число в диапазоне от 0 до 15
+    const number = Math.floor(Math.random()*15)
 
+    // если число кратное 2 -> резолвим промис и возвращаем это число
+    // если число не кратное 2 -> реджектим промис и возвращаем ошибку
+    
+    if(number%2===0){
+    resolve(number); // fulfilled
+    } else {
+       const err =  new RangeError(`Error happenned ${number}`)
+       reject(err)
+    }
+}
+
+p.then((number)=> {
+    console.log(number);
+}, (error)=> {
+    console.log(error);
+})
