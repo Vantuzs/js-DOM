cb56b5443082aad5614ed94634baaea777543803
Vantuz <Shcherbina444@gmail.com>
2024-12-19 13:21:05 +0200

    setTimeout in promise

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-const p = new Promise(executor) // pending
-function executor(resolve,reject){
-    // задача: сгенерировать случайное число в диапазоне от 0 до 15
-    const number = Math.floor(Math.random()*15)
+const p = new Promise(executor); // pending
 
-    // если число кратное 2 -> резолвим промис и возвращаем это число
-    // если число не кратное 2 -> реджектим промис и возвращаем ошибку
-    
-    if(number%2===0){
-    resolve(number); // fulfilled
-    } else {
-       const err =  new RangeError(`Error happenned ${number}`)
-       reject(err)
-    }
+function executor(resolve,reject){
+    setTimeout(() => {
+        resolve('it time');// fulfielled
+    },15000)
 }
 
-p.then((number)=> {
-    console.log(number);
-}, (error)=> {
-    console.log(error);
+const h1 = document.querySelector('#root')
+
+p.then((str)=>{
+    h1.append(str)
 })
