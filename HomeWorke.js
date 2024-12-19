5aabb2b7438852d67d758e5bc6366b82f1011ae9
Vantuz <Shcherbina444@gmail.com>
2024-12-19 14:06:42 +0200

    Promisify loading image

---
+++ HomeWorke.js
@@ -0,0 +1,20 @@
+// //IDEIA
+// const p = new Promise(executor); // pending
+
+// function executor(resolve,reject){
+//     setTimeout(() => {
+//         resolve('it time');// fulfielled
+//     },4000)
+// }
+
+// const h1 = document.querySelector('#root')
+
+// p.then((str)=>{
+//      for(let i = 0;i<15;){
+//             setTimeout(()=>{
+//                 i++
+//                 h1.append(str)
+//             },1000)}
+        
+//     // h1.append(str)
+// })
--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-const p = new Promise(executor); // pending
+function loadImage(src){
+    //1. Создаем картинку
+    const img = document.createElement('img');
+    img.setAttribute('src',src);
+//2. Создаем елемент в рёстке который будет служить нам для того что указывает загружаем мы что-то или нет
+    const h2 = document.createElement('h2');
+    h2.id = 'loading-h2';
+    h2.append('Loading image...');
+    document.body.append(h2);
+    // 3. Промисыфикация - когда мы обворачиваем какойто асинхронный код в промис, для того что бы забеспечити себе удобную работу
+return new Promise((resolve,reject) =>{
+    //ПОдписываем созданую картинку на событие load
+    // Если картинка загрузится - мы резолвим промис с елементом картинки
+    img.addEventListener('load',()=>{
+        resolve(img);
+    });
 
-function executor(resolve,reject){
-    setTimeout(() => {
-        resolve('it time');// fulfielled
-    },15000)
+    //Подписываем картинку на событие error
+    // Если катритка не загрузится - мы реджектим с сообщением про ошибку
+    img.addEventListener('error',()=>{
+        reject('Image can`t be loaded')
+    })
+})
 }
 
-const h1 = document.querySelector('#root')
-
-p.then((str)=>{
-    h1.append(str)
+loadImage('https://cs13.pikabu.ru/post_img/2023/10/28/2/1698456437194820220.jpg')
+.then((img)=>{
+    document.body.append(img)
+},(errorMessage)=>{
+    const h2 = document.createElement('h2');
+    h2.append(errorMessage);
+    document.body.append(h2)
+    
+})
+.finally(()=>{
+    const loadH2 = document.querySelector('#loading-h2');
+    loadH2.remove()
 })
